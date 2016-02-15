3f32faf9f5b811d87daa81f5a87af2ed122f687e
Evgenylad <7384093@gmail.com>
2016-02-15 17:33:44 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-
-
 var link = document.querySelector(".booking-search-btn");
 var popup = document.querySelector(".form-booking");
 
+if (link) {
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.toggle("form-booking-show");
   });
+}
 
 function initialize() {
-var mapOptions = {
-  zoom: 9,
-  center: new google.maps.LatLng(34.8437908,-111.7855004)
+  var mapOptions = {
+    zoom: 9,
+    center: new google.maps.LatLng(34.8437908,-111.7855004)
 }
-var map = new google.maps.Map(document.getElementById("map-canvas"),
-                              mapOptions);
-var myLatLng = new google.maps.LatLng(34.8437908,-111.7855004);
-var beachMarker = new google.maps.Marker({
-  position: myLatLng,
-  map: map,
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+                                mapOptions);
+  var myLatLng = new google.maps.LatLng(34.8437908,-111.7855004);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
 });
 }
 
