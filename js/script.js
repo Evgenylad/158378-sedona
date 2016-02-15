f820d542d48128b73b7d6306239eb1bbfab3719d
Evgenylad <7384093@gmail.com>
2016-02-15 15:16:32 +0300

    Карта через API

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+var link = document.querySelector(".booking-search-btn");
+var popup = document.querySelector(".form-booking");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("form-booking-show");
+  });
+
+  function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(59.9992618,30.3648466)
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+                                mapOptions);
+  var image = "http://i.neoseeker.com/mgv/499846/846/33/15847_179440563793_179437393793_2772323_4548598_n_icon.jpg";
+  var myLatLng = new google.maps.LatLng(59.999,30.364);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
